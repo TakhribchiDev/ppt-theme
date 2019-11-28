f3d6c4745e079b3f6457ccb3d6cf4368607ce624
TakhribchiDev <takhribchidev@gmail.com>
2019-11-28 08:49:56 +0330

    PPT Header completed

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> >
 <head>
+    <title><?php wp_title(); ?></title>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta charset="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
@@ -17,10 +18,34 @@
 </head>
 <body <?php body_class(); ?> >
 
-<div class="container">
-    <div class="row">
-        <div class="ppt-logo col-md-2">
-            <?php svg( 'pardenegar' ); ?>
-        </div><!-- .col-md-2-->
-    </div><!-- .row-->
-</div><!-- .container -->
+<header class="container header-container">
+            <div class="ppt-logo">
+		        <?php svg( 'pardenegar' ); ?>
+            </div><!-- .navbar-brand -->
+
+            <nav class="navbar ppt-navbar">
+	            <?php
+	            wp_nav_menu([
+		            'theme_location' => 'header-primary',
+		            'menu_class' => 'nav navbar-nav ppt-nav',
+		            'walker'    => new Inc\Core\WalkerNav()
+	            ]);
+	            ?>
+            </nav>
+
+            <div class="ppt-search-form">
+		        <?php
+		        get_search_form();
+		        ?>
+            </div><!-- .search-form-container -->
+
+            <div class="ppt-auth">
+                <button class="cart-btn">
+                    <i class="ppt-icon ppt-cart"></i>
+                </button>
+                <button class="profile-btn">
+                    <i class="ppt-icon ppt-profile"></i>
+                    <span><?php esc_html_e( 'حساب کاربری', 'ppttheme' ) ?></span>
+                </button>
+            </div><!-- .auth-container -->
+</header>
