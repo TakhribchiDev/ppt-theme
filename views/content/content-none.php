440f3ab8ba50e67caaeb782cdbeb4f6de407b362
TakhribchiDev <takhribchidev@gmail.com>
2019-11-28 09:10:55 +0330

    PPT theme template files

---
+++ views/content/content-none.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * No posts found page
+ * @package ppttheme
+ */
+?>
+<div>
+	<h1>
+		No post found!
+	</h1>
+</div>
---
+++ views/content/content.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Takhribchi
+ * Date: 27/11/2019
+ * Time: 11:08 PM
+ */
