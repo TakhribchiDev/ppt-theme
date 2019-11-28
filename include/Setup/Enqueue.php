80fea1de49c5ce7b4fe7bd230f6ddd92e856f0d5
TakhribchiDev <takhribchidev@gmail.com>
2019-11-28 08:56:28 +0330

    Added front-end bootstrap and PPT special styles and scripts

--- include/Setup/Enqueue.php
+++ include/Setup/Enqueue.php
@@ -7,10 +7,8 @@ class Enqueue {
 	 * Register default hooks and actions for wordpress
 	 */
 	public function register() {
-
 		add_action( 'admin_enqueue_scripts', [ $this, 'enqueue_admin_scripts' ] );
 		add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_scripts' ] );
-
 	}
 
 	public function enqueue_admin_scripts() {
