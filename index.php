b8b105a76f3e6d33673c59a63152c10913af42ca
TakhribchiDev <takhribchidev@gmail.com>
2019-11-28 09:11:48 +0330

    Added markup for default main content of wordpress

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
 <?php
+/**
+ * PPT Theme general page
+ *
+ * @package ppttheme
+ */
+
 get_header();
+?>
+<section id="primary" class="content-area">
+    <main id="main" class="site-main">
+        <?php
+        if ( have_posts() ) :
+            // Load posts loop
+            while ( have_posts() ) : the_post();
+                get_template_part( 'views/content/content' );
+            endwhile;
+
+            the_posts_pagination();
+        else:
+            // If no posts in the loop, get "No post found"
+            get_template_part( 'views/content/content', 'none' );
+        endif;
+        ?>
+    </main><!-- .site-main -->
+</section><!-- .content-area -->
+<?php
 get_footer();
